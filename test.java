9fed4ff3b77e3bc8f2e4f10dda944fffefce126f
SohamMore-26 <sohammore081@gmail.com>
2023-02-20 16:45:17 +0530

    blah

--- test.java
+++ test.java
@@ -5,13 +5,11 @@ class test
     public static void main (String args[])
     {
         Scanner obj = new Scanner(System.in);
-
         System.out.print("Enter two numbers: ");
         int x = obj.nextInt();
         int y = obj.nextInt();
 
-        System.out.print("SUM: ");
-        System.out.print( x+y);
+        System.out.println("The sum is: " + (x+y));
     }
 }
 
